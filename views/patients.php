3c1936636927b3d794d368452c700696bcdd8166
Martin Mbithi <martdevelopers254@gmail.com>
2022-08-31 18:21:20 +0300

    Implement Patients Details

--- views/patients.php
+++ views/patients.php
@@ -84,13 +84,124 @@ require_once('../app/partials/head.php');
                                     </div>
                                 </div>
                             </div>
+                            <hr>
                             <!-- .page-section -->
                             <div class="page-section">
                                 <!-- .section-block -->
                                 <div class="section-block">
                                     <!-- metric row -->
                                     <div class="metric-row">
+                                        <div class="card col-12">
+                                            <div class="card-body">
+                                                <!-- .table -->
+                                                <table id="dt-responsive" class="table dt-responsive nowrap w-100">
+                                                    <thead>
+                                                        <tr>
+                                                            <th> Number </th>
+                                                            <th> Names </th>
+                                                            <th> ID Number </th>
+                                                            <th> Phone Number </th>
+                                                            <th> Address </th>
+                                                            <th> Manage </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php
+                                                        $patients_sql = mysqli_query($mysqli, "SELECT * FROM users WHERE user_access_level = 'Patient'");
+                                                        if (mysqli_num_rows($patients_sql) > 0) {
+                                                            while ($patients = mysqli_fetch_array($patients_sql)) {
+                                                        ?>
+                                                                <tr>
+                                                                    <td><?php echo $patients['user_number']; ?></td>
+                                                                    <td><?php echo $patients['user_full_names']; ?></td>
+                                                                    <td><?php echo $patients['user_idno']; ?></td>
+                                                                    <td><?php echo $patients['user_phone_number']; ?></td>
+                                                                    <td><?php echo $patients['user_address']; ?></td>
+                                                                    <td class="align-middle text-center">
+                                                                        <a data-toggle="modal" href="#update_<?php echo $patients['user_id']; ?>" class="btn btn-sm btn-icon btn-secondary">
+                                                                            <i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span>
+                                                                        </a>
+                                                                        <a data-toggle="modal" href="#delete_<?php echo $patients['user_id']; ?>" class="btn btn-sm btn-icon btn-secondary"><i class="far fa-trash-alt"></i>
+                                                                            <span class="sr-only">Remove</span>
+                                                                        </a>
+                                                                    </td>
+                                                                </tr>
+                                                                <!-- Update Modal -->
+                                                                <div class="modal fade fixed-right" id="update_<?php echo $patients['user_id']; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+                                                                    <div class="modal-dialog  modal-xl" role="document">
+                                                                        <div class="modal-content">
+                                                                            <div class="modal-header align-items-center">
+                                                                                <div class="modal-title">
+                                                                                    <h6 class="mb-0">Update <?php echo $patients['user_full_names']; ?> Details</h6>
+                                                                                </div>
+                                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                    <span aria-hidden="true">&times;</span>
+                                                                                </button>
+                                                                            </div>
+                                                                            <div class="modal-body">
+                                                                                <form method="post" enctype="multipart/form-data" role="form">
+                                                                                    <div class="row">
+                                                                                        <div class="form-group col-md-8">
+                                                                                            <label for="">Full Names</label>
+                                                                                            <input type="hidden" required name="user_id" value="<?php echo $patients['user_id']; ?>" class="form-control">
+                                                                                            <input type="text" required value="<?php echo $patients['user_full_names']; ?>" name="user_full_names" class="form-control">
+                                                                                        </div>
+                                                                                        <div class="form-group col-md-4">
+                                                                                            <label for="">Number</label>
+                                                                                            <input type="text" required readonly value="<?php echo $patients['user_number']; ?>" name="user_number" class="form-control">
+                                                                                        </div>
+                                                                                        <div class="form-group col-md-6">
+                                                                                            <label for="">National ID Number</label>
+                                                                                            <input type="number" name="user_idno" value="<?php echo $patients['user_idno']; ?>" class="form-control">
+                                                                                        </div>
+                                                                                        <div class="form-group col-md-6">
+                                                                                            <label for="">Phone Number</label>
+                                                                                            <input type="number" required name="user_phone_number" value="<?php echo $patients['user_phone_number']; ?>" class="form-control">
+                                                                                        </div>
+                                                                                        <div class="form-group col-md-12">
+                                                                                            <label for="">Address</label>
+                                                                                            <textarea type="text" name="user_address" rows="2" class="form-control" id="exampleInputEmail1"><?php echo $patients['user_address']; ?></textarea>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                    <div class="text-right">
+                                                                                        <button type="submit" name="Update_Patient" class="btn btn-primary">Update Patient</button>
+                                                                                    </div>
+                                                                                    <br>
+                                                                                </form>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
 
+                                                                <!-- Delete Modal-->
+                                                                <div class="modal fade" id="delete_<?php echo $patients['user_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                        <div class="modal-content">
+                                                                            <div class="modal-header">
+                                                                                <h5 class="modal-title" id="exampleModalLabel">CONFIRM DELETE</h5>
+                                                                                <button type="button" class="close" data-dismiss="modal">
+                                                                                    <span>&times;</span>
+                                                                                </button>
+                                                                            </div>
+                                                                            <form method="POST">
+                                                                                <div class="modal-body text-center text-danger">
+                                                                                    <h4>Delete <?php echo $patients['user_full_names']; ?> Record? </h4>
+                                                                                    <br>
+                                                                                    <!-- Hide This -->
+                                                                                    <input type="hidden" name="user_id" value="<?php echo $patients['user_id']; ?>">
+                                                                                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                                    <input type="submit" name="Delete_User" value="Delete" class="text-center btn btn-danger"><br>
+                                                                                </div>
+                                                                            </form>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                        <?php }
+                                                        } ?>
+                                                    </tbody>
+                                                </table><!-- /.table -->
+                                            </div><!-- /.card-body -->
+                                        </div>
                                     </div><!-- /metric row -->
                                 </div><!-- /.section-block -->
                             </div><!-- /.page-section -->
